0d06765752ad55f24137c5e7ca85f9d7aefad64a
mstaczek <matemmatem2000@gmail.com>
2023-05-24 17:32:23 +0200

    add cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_ToSJn4rtf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 't3hpg100c6ufatagd970tfjnn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: ' us-east-1 ' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://slsg0n4r62.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
